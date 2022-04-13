f4460acf66dcee62763a9c9ad7f4a611bede5982
Ricky Leverence <ricky.leverence@gmail.com>
2022-04-13 12:13:47 -0400

    Initial awful commit. Boxes draw, things move, code is nasty.

---
+++ PrevueGuide/Generators.cs
@@ -0,0 +1,160 @@
+using System.Runtime.InteropServices;
+using SDL2;
+
+namespace PrevueGuide;
+
+public static class Generators
+{
+    public static IntPtr LoadImageToTexture(IntPtr renderer, string filename)
+    {
+        // If I decide to pack everything into a zip or something...
+        // var src = SDL_RWFromMem(...)
+        // SDL_image.IMG_Load_RW(src, 1)
+
+        var surface = SDL_image.IMG_Load(filename);
+        var texture = SDL.SDL_CreateTextureFromSurface(renderer, surface);
+        _ = SDL.SDL_SetTextureBlendMode(texture, SDL.SDL_BlendMode.SDL_BLENDMODE_BLEND);
+        SDL.SDL_FreeSurface(surface);
+        return texture;
+    }
+
+    public static IntPtr GenerateDropShadowText(IntPtr renderer, IntPtr font, string text,
+        SDL.SDL_Color fontColor, int scale = 1)
+    {
+        const int horizontalOffset = 1;
+        const int verticalOffset = 1;
+
+        var black = new SDL.SDL_Color { a = 255, r = 17, g = 17, b = 17 };
+
+        SDL_ttf.TTF_SetFontOutline(font, scale);
+        var outlineSurface = SDL_ttf.TTF_RenderText_Blended(font, text, black);
+        var outlineTexture = SDL.SDL_CreateTextureFromSurface(renderer, outlineSurface);
+
+        SDL_ttf.TTF_SetFontOutline(font, 0);
+        var shadowSurface = SDL_ttf.TTF_RenderText_Blended(font, text, black);
+        var shadowTexture = SDL.SDL_CreateTextureFromSurface(renderer, shadowSurface);
+        var mainSurface = SDL_ttf.TTF_RenderText_Blended(font, text, fontColor);
+        var mainTexture = SDL.SDL_CreateTextureFromSurface(renderer, mainSurface);
+
+        var outlineSdlSurface = Marshal.PtrToStructure<SDL.SDL_Surface>(outlineSurface);
+
+        // Generate a texture that's 1 * scale wider than the outline, as its going to
+        // first be used to draw the drop shadow at a 1 x 1 offset.
+        var resultTexture = SDL.SDL_CreateTexture(renderer, SDL.SDL_PIXELFORMAT_RGBA8888,
+            (int)SDL.SDL_TextureAccess.SDL_TEXTUREACCESS_TARGET, (outlineSdlSurface.w + 1 * scale),
+            outlineSdlSurface.h + 1 * scale);
+        _ = SDL.SDL_SetTextureBlendMode(resultTexture, SDL.SDL_BlendMode.SDL_BLENDMODE_BLEND);
+
+        // Switch to the texture for rendering.
+        _ = SDL.SDL_SetRenderTarget(renderer, resultTexture);
+
+        // Draw clock, black shadow outline
+        _ = SDL.SDL_QueryTexture(outlineTexture, out _, out _, out var w1, out var h1);
+        var dstRect1 = new SDL.SDL_Rect { h = h1, w = w1, x = horizontalOffset * scale, y = verticalOffset * scale };
+        SDL.SDL_RenderCopy(renderer, outlineTexture, IntPtr.Zero, ref dstRect1);
+
+        // Draw clock, black shadow main
+        _ = SDL.SDL_QueryTexture(shadowTexture, out _, out _, out var w2, out var h2);
+        var dstRect2 = new SDL.SDL_Rect
+            { h = h2, w = w2, x = (horizontalOffset + 2) * scale, y = (verticalOffset + 2) * scale };
+        SDL.SDL_RenderCopy(renderer, shadowTexture, IntPtr.Zero, ref dstRect2);
+
+        // Draw clock, black outline
+        _ = SDL.SDL_QueryTexture(outlineTexture, out _, out _, out var w3, out var h3);
+        var dstRect3 = new SDL.SDL_Rect
+            { h = h3, w = w3, x = (horizontalOffset - 1) * scale, y = (verticalOffset - 1) * scale };
+        SDL.SDL_RenderCopy(renderer, outlineTexture, IntPtr.Zero, ref dstRect3);
+
+        // Draw clock, main without outline
+        _ = SDL.SDL_QueryTexture(mainTexture, out _, out _, out var w4, out var h4);
+        var dstRect4 = new SDL.SDL_Rect { h = h4, w = w4, x = horizontalOffset * scale, y = verticalOffset * scale };
+        SDL.SDL_RenderCopy(renderer, mainTexture, IntPtr.Zero, ref dstRect4);
+
+        // Switch back to the main render target.
+        SDL.SDL_SetRenderTarget(renderer, IntPtr.Zero);
+
+        // Clean up
+        SDL.SDL_DestroyTexture(outlineTexture);
+        SDL.SDL_DestroyTexture(shadowTexture);
+        SDL.SDL_DestroyTexture(mainTexture);
+        SDL.SDL_FreeSurface(mainSurface);
+        SDL.SDL_FreeSurface(shadowSurface);
+        SDL.SDL_FreeSurface(outlineSurface);
+
+        return resultTexture;
+    }
+
+    public static IntPtr GenerateFrame(string baseAssetsDirectory, IntPtr renderer,
+        int width, int height, SDL.SDL_Color backgroundColor, int scale = 1)
+    {
+        var upperLeft = LoadImageToTexture(renderer, $"{baseAssetsDirectory}/frame_upper_left_2x_smooth.png");
+        var upperRight = LoadImageToTexture(renderer, $"{baseAssetsDirectory}/frame_upper_right_2x_smooth.png");
+        var lowerLeft = LoadImageToTexture(renderer, $"{baseAssetsDirectory}/frame_lower_left_2x_smooth.png");
+        var lowerRight = LoadImageToTexture(renderer, $"{baseAssetsDirectory}/frame_lower_right_2x_smooth.png");
+        var left = LoadImageToTexture(renderer, $"{baseAssetsDirectory}/frame_left_2x.png");
+        var right = LoadImageToTexture(renderer, $"{baseAssetsDirectory}/frame_right_2x.png");
+        var upper = LoadImageToTexture(renderer, $"{baseAssetsDirectory}/frame_upper_2x.png");
+        var lower = LoadImageToTexture(renderer, $"{baseAssetsDirectory}/frame_lower_2x.png");
+
+        var resultTexture = SDL.SDL_CreateTexture(renderer, SDL.SDL_PIXELFORMAT_RGBA8888,
+            (int)SDL.SDL_TextureAccess.SDL_TEXTUREACCESS_TARGET, width * scale,
+            height * scale);
+        _ = SDL.SDL_SetTextureBlendMode(resultTexture, SDL.SDL_BlendMode.SDL_BLENDMODE_BLEND);
+
+        // Switch to the texture for rendering.
+        _ = SDL.SDL_SetRenderTarget(renderer, resultTexture);
+
+        _ = SDL.SDL_SetRenderDrawColor(renderer, backgroundColor.r, backgroundColor.g, backgroundColor.b,
+            backgroundColor.a);
+        _ = SDL.SDL_RenderClear(renderer);
+
+        _ = SDL.SDL_QueryTexture(left, out uint _, out int _, out int lw, out int lh);
+        var lDstRect = new SDL.SDL_Rect { h = (height * scale), w = lw, x = 0, y = 0 };
+        _ = SDL.SDL_RenderCopy(renderer, left, IntPtr.Zero, ref lDstRect);
+
+        _ = SDL.SDL_QueryTexture(right, out uint _, out int _, out int rw, out int rh);
+        var rDstRect = new SDL.SDL_Rect { h = (height * scale), w = rw, x = ((width * scale) - rw), y = 0 };
+        _ = SDL.SDL_RenderCopy(renderer, right, IntPtr.Zero, ref rDstRect);
+
+        _ = SDL.SDL_QueryTexture(upper, out uint _, out int _, out int uw, out int uh);
+        var uDstRect = new SDL.SDL_Rect { h = uh, w = (width * scale), x = 0, y = 0 };
+        _ = SDL.SDL_RenderCopy(renderer, upper, IntPtr.Zero, ref uDstRect);
+
+        _ = SDL.SDL_QueryTexture(lower, out uint _, out int _, out int lowerWidth, out int lowerHeight);
+        var lowerDstRect = new SDL.SDL_Rect
+            { h = lowerHeight, w = (width * scale), x = 0, y = (height * scale) - lowerHeight };
+        _ = SDL.SDL_RenderCopy(renderer, lower, IntPtr.Zero, ref lowerDstRect);
+
+        _ = SDL.SDL_QueryTexture(upperLeft, out uint _, out int _, out int ulw, out int ulh);
+        var ulDstRect = new SDL.SDL_Rect { h = ulh, w = ulw, x = 0, y = 0 };
+        _ = SDL.SDL_RenderCopy(renderer, upperLeft, IntPtr.Zero, ref ulDstRect);
+
+        _ = SDL.SDL_QueryTexture(upperRight, out uint _, out int _, out int urw, out int urh);
+        var urDstRect = new SDL.SDL_Rect { h = urh, w = urw, x = (width * scale) - urw, y = 0 };
+        _ = SDL.SDL_RenderCopy(renderer, upperRight, IntPtr.Zero, ref urDstRect);
+
+        _ = SDL.SDL_QueryTexture(lowerLeft, out _, out _, out var lowerLeftWidth, out var lowerLeftHeight);
+        var lowerLeftDstRect = new SDL.SDL_Rect
+            { h = lowerLeftHeight, w = lowerLeftWidth, x = 0, y = (height * scale) - lowerLeftHeight };
+        _ = SDL.SDL_RenderCopy(renderer, lowerLeft, IntPtr.Zero, ref lowerLeftDstRect);
+
+        _ = SDL.SDL_QueryTexture(lowerRight, out _, out _, out var lrw, out var lrh);
+        var lowerRightDstRect = new SDL.SDL_Rect
+            { h = urh, w = urw, x = (width * scale) - lrw, y = (height * scale) - lrh };
+        _ = SDL.SDL_RenderCopy(renderer, lowerRight, IntPtr.Zero, ref lowerRightDstRect);
+
+        // Switch back to the main render target.
+        _ = SDL.SDL_SetRenderTarget(renderer, IntPtr.Zero);
+
+        SDL.SDL_DestroyTexture(upperLeft);
+        SDL.SDL_DestroyTexture(upperRight);
+        SDL.SDL_DestroyTexture(lowerLeft);
+        SDL.SDL_DestroyTexture(lowerRight);
+        SDL.SDL_DestroyTexture(left);
+        SDL.SDL_DestroyTexture(right);
+        SDL.SDL_DestroyTexture(upper);
+        SDL.SDL_DestroyTexture(lower);
+
+        return resultTexture;
+    }
+}
---
+++ PrevueGuide/Program.cs
@@ -0,0 +1,246 @@
+﻿using System.Diagnostics;
+using PrevueGuide;
+using SDL2;
+using static SDL2.SDL;
+
+const int windowWidth = 716;
+const int windowHeight = 436;
+
+IntPtr window;
+IntPtr renderer;
+IntPtr prevueGrid;
+
+DateTime time = DateTime.UnixEpoch;
+
+IntPtr timeTexture = IntPtr.Zero;
+IntPtr channelFrameTexture = IntPtr.Zero;
+IntPtr clockFrameTexture = IntPtr.Zero;
+IntPtr timeboxFrameTexture = IntPtr.Zero;
+IntPtr timeboxLastFrameTexture = IntPtr.Zero;
+
+IntPtr bigFrameTexture;
+IntPtr bigFrameText;
+
+var scale = 2;
+var baseAssetsDirectory = "/Users/RJ/Desktop";
+var running = true;
+
+var clockBackgroundColor = new SDL_Color { a = 255, r = 34, g = 41, b = 141 };
+
+var scrollingTest = 0;
+
+Setup();
+
+while (running)
+{
+    PollEvents();
+    Render();
+}
+
+CleanUp();
+
+// Setup all of the SDL resources we'll need to display a window.
+void Setup()
+{
+    // Initializes SDL.
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        Console.WriteLine($"There was an issue initializing SDL. {SDL_GetError()}");
+    }
+
+    // Create a new window given a title, size, and passes it a flag indicating it should be shown.
+    window = SDL_CreateWindow(
+        "",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        windowWidth,
+        windowHeight,
+        SDL_WindowFlags.SDL_WINDOW_SHOWN | SDL_WindowFlags.SDL_WINDOW_ALLOW_HIGHDPI);
+
+    if (window == IntPtr.Zero)
+    {
+        Console.WriteLine($"There was an issue creating the window. {SDL_GetError()}");
+    }
+
+    // Creates a new SDL hardware renderer using the default graphics device with VSYNC enabled.
+    renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RendererFlags.SDL_RENDERER_ACCELERATED |
+        SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
+
+    if (renderer == IntPtr.Zero)
+    {
+        Console.WriteLine($"There was an issue creating the renderer. {SDL_GetError()}");
+    }
+
+    _ = SDL_SetRenderDrawBlendMode(renderer, SDL_BlendMode.SDL_BLENDMODE_BLEND);
+
+    SDL_ttf.TTF_Init();
+    prevueGrid = SDL_ttf.TTF_OpenFont("/Users/rj/Library/Fonts/PrevueGrid.ttf", 25 * scale); // Maybe 50? Test.
+
+    SDL_image.IMG_Init(SDL_image.IMG_InitFlags.IMG_INIT_PNG);
+
+    clockFrameTexture = Generators.GenerateFrame(baseAssetsDirectory, renderer, 144, 34,
+        clockBackgroundColor, scale);
+    timeboxFrameTexture = Generators.LoadImageToTexture(renderer, $"{baseAssetsDirectory}/timebox_frame_2x_smooth.png");
+    timeboxLastFrameTexture =
+        Generators.LoadImageToTexture(renderer, $"{baseAssetsDirectory}/timebox_last_frame_2x_smooth.png");
+    channelFrameTexture = Generators.LoadImageToTexture(renderer, $"{baseAssetsDirectory}/channel_frame_2x_smooth.png");
+    bigFrameTexture = Generators.GenerateFrame(baseAssetsDirectory, renderer, 300, 80,
+        new SDL_Color { a = 255, r = 192, g = 0, b = 0 }, scale);
+    bigFrameText = Generators.GenerateDropShadowText(renderer, prevueGrid, "PrrRRrreeVuuEEE",
+        new SDL_Color { a = 255, r = 203, g = 209, b = 0 }, scale);
+}
+
+// Checks to see if there are any events to be processed.
+void PollEvents()
+{
+    // Check to see if there are any events and continue to do so until the queue is empty.
+    while (SDL_PollEvent(out var sdlEvent) == 1)
+    {
+        running = sdlEvent.type switch
+        {
+            SDL_EventType.SDL_QUIT => false,
+            _ => running
+        };
+    }
+}
+
+// Renders to the window.
+void Render()
+{
+    var stopWatch = Stopwatch.StartNew();
+
+    _ = SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    _ = SDL_RenderClear(renderer);
+
+    // Cheap and cheating.
+    var now = DateTime.Now;
+    if (time.Second != now.Second)
+    {
+        time = now;
+        timeTexture = Generators.GenerateDropShadowText(renderer, prevueGrid, now.ToString("h:mm:ss"),
+            new SDL_Color { a = 255, r = 170, g = 170, b = 170 }, scale);
+    }
+
+    // Render the entirety of the grid to its own texture,
+    // that way we can properly cut off content from the upper panel.
+    // Also allows us to shift the grid up and down by simply changing the texture Y
+    // by a few rows down.
+    var gridTexture = SDL_CreateTexture(renderer, SDL.SDL_PIXELFORMAT_RGBA8888,
+        (int)SDL_TextureAccess.SDL_TEXTUREACCESS_TARGET, windowWidth * scale,
+        windowHeight * scale);
+    _ = SDL_SetTextureBlendMode(gridTexture, SDL.SDL_BlendMode.SDL_BLENDMODE_BLEND);
+
+    // Switch to the texture for rendering.
+    _ = SDL_SetRenderTarget(renderer, gridTexture);
+
+    _ = SDL_SetRenderDrawColor(renderer, 4, 0, 89, 255);
+    _ = SDL_RenderClear(renderer);
+
+    // Quick guess. 110 frames for a full grid push @ 112 (2x) / 56 (1x) height.
+    // That means roughly 2 frames per pixel going up.
+    scrollingTest += 1;
+    if (scrollingTest >= 370)
+        scrollingTest = 0;
+    var testingOffset = (scrollingTest / 2);
+
+    var frameX = 152;
+    var frameY = 150;
+
+    _ = SDL_QueryTexture(bigFrameTexture, out _, out _, out var bfWidth, out var bfHeight);
+    var bfDstRect = new SDL_Rect
+        { h = bfHeight, w = bfWidth, x = frameX * scale, y = ((frameY - testingOffset) * scale) };
+    _ = SDL_RenderCopy(renderer, bigFrameTexture, IntPtr.Zero, ref bfDstRect);
+
+    _ = SDL_QueryTexture(bigFrameText, out _, out _, out var bftWidth, out var bftHeight);
+    var bftDstRect = new SDL_Rect
+        { h = bftHeight, w = bftWidth, x = (frameX + 16) * scale, y = (frameY + 16 - testingOffset) * scale };
+    _ = SDL_RenderCopy(renderer, bigFrameText, IntPtr.Zero, ref bftDstRect);
+
+    // Draw the clock frame.
+    _ = SDL_QueryTexture(clockFrameTexture, out _, out _, out var clockFrameWidth, out var clockFrameHeight);
+    var clockFrameDstRect = new SDL_Rect { h = clockFrameHeight, w = clockFrameWidth, x = 8 * scale, y = 0 };
+    _ = SDL_RenderCopy(renderer, clockFrameTexture, IntPtr.Zero, ref clockFrameDstRect);
+
+    // Draw the time boxes.
+    {
+        // First two time boxes.
+        _ = SDL_QueryTexture(timeboxFrameTexture, out uint _, out int _, out int tbw, out int tbh);
+        var dstRect1 = new SDL_Rect { h = tbh, w = tbw, x = 152 * scale, y = 0 };
+        _ = SDL_RenderCopy(renderer, timeboxFrameTexture, IntPtr.Zero, ref dstRect1);
+        var dstRect2 = new SDL_Rect { h = tbh, w = tbw, x = 324 * scale, y = 0 };
+        _ = SDL_RenderCopy(renderer, timeboxFrameTexture, IntPtr.Zero, ref dstRect2);
+
+        // Last one.
+        _ = SDL_QueryTexture(timeboxLastFrameTexture, out uint _, out int _, out int tblw, out int tblh);
+        var dstRect = new SDL_Rect { h = tblh, w = tblw, x = 496 * scale, y = 0 };
+        _ = SDL_RenderCopy(renderer, timeboxLastFrameTexture, IntPtr.Zero, ref dstRect);
+    }
+
+    var horizontalOffset = 62;
+    var verticalOffset = 7;
+
+    if (now.Hour is 00 or >= 10)
+        horizontalOffset -= 12;
+
+    // Draw 3 channel frames.
+    {
+        var baseChannelY = 34;
+        _ = SDL_QueryTexture(channelFrameTexture, out _, out _, out var w, out var h);
+        var dstRect1 = new SDL_Rect
+            { h = h, w = w, x = 8 * scale, y = baseChannelY * scale };
+        _ = SDL_RenderCopy(renderer, channelFrameTexture, IntPtr.Zero, ref dstRect1);
+        var dstRect2 = new SDL_Rect
+            { h = h, w = w, x = 8 * scale, y = (baseChannelY + 56) * scale };
+        _ = SDL_RenderCopy(renderer, channelFrameTexture, IntPtr.Zero, ref dstRect2);
+        var dstRect3 = new SDL_Rect
+            { h = h, w = w, x = 8 * scale, y = (baseChannelY + 112) * scale };
+        _ = SDL_RenderCopy(renderer, channelFrameTexture, IntPtr.Zero, ref dstRect3);
+    }
+
+    _ = SDL_QueryTexture(timeTexture, out _, out _, out var timeWidth, out var timeHeight);
+    var timeDstRect = new SDL_Rect
+        { h = timeHeight, w = timeWidth, x = (horizontalOffset - 1) * scale, y = (verticalOffset - 1) * scale };
+    _ = SDL_RenderCopy(renderer, timeTexture, IntPtr.Zero, ref timeDstRect);
+
+    // Switch to the primary surface/target
+    _ = SDL_SetRenderTarget(renderer, IntPtr.Zero);
+
+    // Draw the grid
+    _ = SDL_QueryTexture(gridTexture, out _, out _, out var gridTextureWidth, out var gridTextureHeight);
+    var gridDstRect = new SDL_Rect { h = gridTextureHeight, w = gridTextureWidth, x = 0, y = (227 * scale) };
+    _ = SDL_RenderCopy(renderer, gridTexture, IntPtr.Zero, ref gridDstRect);
+    SDL_DestroyTexture(gridTexture);
+
+    // Switches out the currently presented render surface with the one we just did work on.
+    SDL_RenderPresent(renderer);
+
+    // Simulate 30 fps.
+    stopWatch.Stop();
+    var duration = (33 - stopWatch.ElapsedMilliseconds);
+    if (duration > 0)
+        Thread.Sleep((int)duration);
+}
+
+// Clean up the resources that were created.
+void CleanUp()
+{
+    SDL_DestroyTexture(timeTexture);
+
+    SDL_DestroyTexture(clockFrameTexture);
+    SDL_DestroyTexture(timeboxFrameTexture);
+    SDL_DestroyTexture(timeboxLastFrameTexture);
+
+    SDL_DestroyTexture(bigFrameTexture);
+    SDL_DestroyTexture(bigFrameText);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_ttf.TTF_CloseFont(prevueGrid);
+    SDL_ttf.TTF_Quit();
+
+    SDL_Quit();
+}
