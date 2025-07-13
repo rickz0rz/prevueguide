9765c3cb434c59e5dbc7e02e320a5dcb3d7a2441
Ricky Leverence Jr <ricky.leverence@gmail.com>
2025-07-12 20:18:51 -0400

    Cleanup.

--- PrevueGuide/Guide.cs
+++ PrevueGuide/Guide.cs
@@ -20,35 +20,39 @@ namespace PrevueGuide;
 // Don't rerender every element if they have not changed.
 // Allow listings provider to cache listings if they're not updated and silently update them in the background
 //     if they need to be...? Maybe have it on a timer and do it async.
+// yank esq specific code out of guide.cs
 
 public class Guide : IDisposable
 {
+    private const string FiraCodeFontKey = "FiraCode";
+    private const string FpsTextureKey = "fps";
+    private const string GuideTextureKey = "guide";
+
     private readonly ILogger _logger;
     private readonly TextureManager _textureManager;
     private readonly FontManager _fontManager;
     private readonly IGuideThemeProvider _esquireGuideThemeProvider;
+    private readonly ContainedLineLogger _containedLineLogger;
+    private readonly List<int> _frameTimes;
+    private readonly Queue<Texture> _rowsTextureQueue = new();
 
-    private List<int> _frameTimes;
     private int _updateFPSFrameCounter;
-
     private IntPtr _window;
     private IntPtr _renderer;
     private bool _running;
     private bool _showLogs;
     private bool _showFps;
-    private bool _highDpi = true;
     private bool _vsync = true;
     private bool _fullscreen;
-    private bool _thirtyFpsLimit = false;
+    private bool _thirtyFpsLimit;
     private FullscreenMode _currentFullscreenMode;
-
-    private ContainedLineLogger _containedLineLogger;
     private long _lastLinesLoggedCount = -1;
-    private float _guideRowBaseY = 0f;
-    private float _scrollSpeed = 0f;
+    private float _guideRowBaseY;
+    private float _scrollSpeed;
+
+    private bool _highDpi = true;
 
     private IEnumerator<Texture> _rowsTextureSource;
-    private Queue<Texture> _rowsTextureQueue = new();
     private ChannelsDVRListingsDataProvider provider;
 
     public Guide(ILogger logger)
@@ -65,9 +69,7 @@ public class Guide : IDisposable
         _fontManager = new FontManager(logger);
         _esquireGuideThemeProvider = new EsquireGuideThemeProvider(_logger);
         _currentFullscreenMode = _esquireGuideThemeProvider.DefaultFullscreenMode;
-        _frameTimes  = [];
-
-        // For rendering on-screen logs.
+        _frameTimes = [];
         _containedLineLogger = ContainedLineLoggerProvider.Logger;
     }
 
@@ -104,12 +106,12 @@ public class Guide : IDisposable
 
     private void Setup()
     {
-        // var scaledWindow = (int)(_guideThemeProvider.DefaultWindowHeight * _guideThemeProvider.ScaleRatio);
-
-        _window = SDL.CreateWindow("Prevue Guide",
+        _window = SDL.CreateWindow("Guide",
             _esquireGuideThemeProvider.DefaultWindowWidth,
             _esquireGuideThemeProvider.DefaultWindowHeight,
-            _highDpi ? SDL.WindowFlags.HighPixelDensity | SDL.WindowFlags.Metal : SDL.WindowFlags.Metal);
+            _highDpi
+                ? SDL.WindowFlags.HighPixelDensity | SDL.WindowFlags.InputFocus
+                : SDL.WindowFlags.InputFocus);
 
         if (_window == IntPtr.Zero)
         {
@@ -126,7 +128,6 @@ public class Guide : IDisposable
 
         _esquireGuideThemeProvider.SetRenderer(_renderer);
 
-        var now = DateTime.Now;
         provider = new ChannelsDVRListingsDataProvider(_logger, "http://192.168.0.195:8089");
         provider.PrevueChannelNumber = 1;
 
@@ -152,66 +153,62 @@ public class Guide : IDisposable
         Configuration.Scale = (int)float.Floor(smallerScale);
         _logger.LogInformation(@"[Window] Scale: {Scale}x", Configuration.Scale);
 
-        if (_currentFullscreenMode == FullscreenMode.Letterbox)
+        switch (_currentFullscreenMode)
         {
-            _logger.LogInformation($"Using fullscreen mode: {FullscreenMode.Letterbox}");
+            case FullscreenMode.Letterbox:
+                _logger.LogInformation($"Using fullscreen mode: {FullscreenMode.Letterbox}");
 
-            Configuration.DrawableWidth = Configuration.Scale * _esquireGuideThemeProvider.DefaultWindowWidth;
-            Configuration.DrawableHeight = Configuration.Scale * _esquireGuideThemeProvider.DefaultWindowHeight;
-            _logger.LogInformation(@"[Window] Drawable Size: {Width} x {Height}", Configuration.DrawableWidth,
-                Configuration.DrawableHeight);
+                Configuration.DrawableWidth = Configuration.Scale * _esquireGuideThemeProvider.DefaultWindowWidth;
+                Configuration.DrawableHeight = Configuration.Scale * _esquireGuideThemeProvider.DefaultWindowHeight;
+                _logger.LogInformation(@"[Window] Drawable Size: {Width} x {Height}", Configuration.DrawableWidth,
+                    Configuration.DrawableHeight);
 
-            Configuration.X = (windowWidthPixels - Configuration.DrawableWidth) / 2;
-            Configuration.Y = (windowHeightPixels - Configuration.DrawableHeight) / 2;
-            _logger.LogInformation(@"[Window] Offset: {Width} x {Height}", Configuration.X, Configuration.Y);
+                Configuration.X = (windowWidthPixels - Configuration.DrawableWidth) / 2;
+                Configuration.Y = (windowHeightPixels - Configuration.DrawableHeight) / 2;
+                _logger.LogInformation(@"[Window] Offset: {Width} x {Height}", Configuration.X, Configuration.Y);
 
-            Configuration.RenderedWidth = Configuration.DrawableWidth;
-            Configuration.RenderedHeight = Configuration.DrawableHeight;
-        }
-        else if (_currentFullscreenMode == FullscreenMode.ZoomedFill)
-        {
-            _logger.LogInformation($"Using fullscreen mode: {FullscreenMode.ZoomedFill}");
+                Configuration.RenderedWidth = Configuration.DrawableWidth;
+                Configuration.RenderedHeight = Configuration.DrawableHeight;
+                break;
+            case FullscreenMode.ZoomedFill:
+                _logger.LogInformation($"Using fullscreen mode: {FullscreenMode.ZoomedFill}");
 
-            Configuration.DrawableWidth = Configuration.Scale * _esquireGuideThemeProvider.DefaultWindowWidth;
-            Configuration.DrawableHeight = Configuration.Scale * _esquireGuideThemeProvider.DefaultWindowHeight;
-            _logger.LogInformation(@"[Window] Drawable Size: {Width} x {Height}", Configuration.DrawableWidth,
-                Configuration.DrawableHeight);
+                Configuration.DrawableWidth = Configuration.Scale * _esquireGuideThemeProvider.DefaultWindowWidth;
+                Configuration.DrawableHeight = Configuration.Scale * _esquireGuideThemeProvider.DefaultWindowHeight;
+                _logger.LogInformation(@"[Window] Drawable Size: {Width} x {Height}", Configuration.DrawableWidth,
+                    Configuration.DrawableHeight);
 
-            Configuration.RenderedWidth = (int)(smallerScale * _esquireGuideThemeProvider.DefaultWindowWidth);
-            Configuration.RenderedHeight = (int)(smallerScale * _esquireGuideThemeProvider.DefaultWindowHeight);
-            _logger.LogInformation(@"[Window] Rendered: {Width} x {Height}", Configuration.RenderedWidth, Configuration.RenderedHeight);
+                Configuration.RenderedWidth = (int)(smallerScale * _esquireGuideThemeProvider.DefaultWindowWidth);
+                Configuration.RenderedHeight = (int)(smallerScale * _esquireGuideThemeProvider.DefaultWindowHeight);
+                _logger.LogInformation(@"[Window] Rendered: {Width} x {Height}", Configuration.RenderedWidth, Configuration.RenderedHeight);
 
-            Configuration.X = (windowWidthPixels - Configuration.RenderedWidth) / 2;
-            Configuration.Y = (windowHeightPixels - Configuration.RenderedHeight) / 2;
-            _logger.LogInformation(@"[Window] Offset: {Width} x {Height}", Configuration.X, Configuration.Y);
-        }
-        else // Default: ScaledFill
-        {
-            _logger.LogInformation($"Using fullscreen mode: {FullscreenMode.ScaledFill}");
+                Configuration.X = (windowWidthPixels - Configuration.RenderedWidth) / 2;
+                Configuration.Y = (windowHeightPixels - Configuration.RenderedHeight) / 2;
+                _logger.LogInformation(@"[Window] Offset: {Width} x {Height}", Configuration.X, Configuration.Y);
+                break;
+            default:
+                // Default: ScaledFill
+                _logger.LogInformation($"Using fullscreen mode: {FullscreenMode.ScaledFill}");
 
-            Configuration.DrawableWidth = windowWidthPixels;
-            Configuration.DrawableHeight = windowHeightPixels;
-            _logger.LogInformation(@"[Window] Drawable Size: {Width} x {Height}", Configuration.DrawableWidth,
-                Configuration.DrawableHeight);
+                Configuration.DrawableWidth = windowWidthPixels;
+                Configuration.DrawableHeight = windowHeightPixels;
+                _logger.LogInformation(@"[Window] Drawable Size: {Width} x {Height}", Configuration.DrawableWidth,
+                    Configuration.DrawableHeight);
 
-            Configuration.X = 0;
-            Configuration.Y = 0;
-            _logger.LogInformation(@"[Window] Offset: {Width} x {Height}", Configuration.X, Configuration.Y);
+                Configuration.X = 0;
+                Configuration.Y = 0;
+                _logger.LogInformation(@"[Window] Offset: {Width} x {Height}", Configuration.X, Configuration.Y);
 
-            Configuration.RenderedWidth = Configuration.DrawableWidth;
-            Configuration.RenderedHeight = Configuration.DrawableHeight;
+                Configuration.RenderedWidth = Configuration.DrawableWidth;
+                Configuration.RenderedHeight = Configuration.DrawableHeight;
+                break;
         }
     }
 
     private void SetFullscreen()
     {
-        // TODO: When in fullscreen, calculate a new "scale" from our requested resolution,
-        // versus the actual resolution so we can keep the guide stuff rendering at the proper size.
         _logger.LogInformation($"SDL: Setting fullscreen to: {_fullscreen}");
         _ = SDL.SetWindowFullscreen(_window, _fullscreen);
-
-        // SetScaleFromWindowSize();
-        // TestGenerateFrameTexture();
     }
 
     private void SetVSync()
@@ -235,20 +232,18 @@ public class Guide : IDisposable
                 {
                     case SDL.Keycode.Equals:
                         _scrollSpeed += 0.125f;
+                        _logger.LogInformation("Scroll speed increased: {Speed}", _scrollSpeed);
                         break;
                     case SDL.Keycode.Minus:
                         _scrollSpeed -= 0.125f;
                         if (_scrollSpeed < 0)
                             _scrollSpeed = 0f;
+                        _logger.LogInformation("Scroll speed reduced: {Speed}", _scrollSpeed);
                         break;
                     case SDL.Keycode.D:
-
-                        for (var i = 0; i < 3; i++)
-                        {
-                            if (_rowsTextureQueue.Any())
-                                _ = _rowsTextureQueue.Dequeue();
-                        }
-                        CheckIfQueueFilled();
+                        _ = _rowsTextureQueue.Dequeue();
+                        _logger.LogInformation("Destroying current first row texture");
+                        FillRowTextureQueue();
                         break;
                     case SDL.Keycode.F:
                         _fullscreen = !_fullscreen;
@@ -279,6 +274,7 @@ public class Guide : IDisposable
                         _logger.LogInformation("30 FPS Limit: {ThirtyFpsLimit}", _thirtyFpsLimit);
                         break;
                     case SDL.Keycode.Q:
+                        _logger.LogInformation("Quitting");
                         _running = false;
                         break;
                     case SDL.Keycode.P:
@@ -303,17 +299,8 @@ public class Guide : IDisposable
     {
         try
         {
-            _textureManager.PurgeTexture("guide");
+            _textureManager[GuideTextureKey] = new Texture(_renderer, Configuration.UnscaledDrawableWidth, Configuration.UnscaledDrawableHeight);
 
-            /*
-            listings = listings.Where(listing => listing is ChannelListing)
-                .Select(listing => listing as ChannelListing)
-                .Where(listing => listing.Programs.First().IsMovie);
-                */
-
-            _textureManager["guide"] = new Texture(_renderer, Configuration.UnscaledDrawableWidth, Configuration.UnscaledDrawableHeight);
-
-            // Purge the queue
             while (_rowsTextureQueue.Count > 0)
             {
                 _rowsTextureQueue.Dequeue().Dispose();
@@ -322,7 +309,7 @@ public class Guide : IDisposable
             var listings = provider.GetEntries().ToBlockingEnumerable();
             _rowsTextureSource = _esquireGuideThemeProvider.GenerateRows(listings).GetEnumerator();
 
-            CheckIfQueueFilled();
+            FillRowTextureQueue();
         }
         catch (Exception ex)
         {
@@ -332,23 +319,11 @@ public class Guide : IDisposable
 
     private int GetQueueHeight()
     {
-        var height = 0;
-
-        foreach (var texture in _rowsTextureQueue)
-        {
-            SDL.GetTextureSize(texture.SdlTexture, out _, out var h);
-            height += (int)h;
-        }
-
-        return height;
-
-        /*
         return _rowsTextureQueue.Select(texture =>
         {
             SDL.GetTextureSize(texture.SdlTexture, out _, out var h);
             return (int)h;
         }).Sum();
-        */
     }
 
     // Hacky thing: How do I add extra records to the queue?
@@ -359,9 +334,9 @@ public class Guide : IDisposable
         _ = InternalSDL3.SetRenderDrawColor(_renderer, Colors.Black0);
         _ = SDL.RenderClear(_renderer);
 
-        if (_textureManager["guide"] != null)
+        if (_textureManager[GuideTextureKey] != null)
         {
-            using (_ = new RenderingTarget(_renderer, _textureManager["guide"]))
+            using (_ = new RenderingTarget(_renderer, _textureManager[GuideTextureKey]))
             {
                 InternalSDL3.SetRenderDrawColor(_renderer, _esquireGuideThemeProvider.DefaultGuideBackground);
                 SDL.RenderClear(_renderer);
@@ -390,7 +365,7 @@ public class Guide : IDisposable
                 if (_guideRowBaseY >= firstH)
                 {
                     _ = _rowsTextureQueue.Dequeue();
-                    CheckIfQueueFilled();
+                    FillRowTextureQueue();
                     _guideRowBaseY = 0f;
                 }
             }
@@ -405,7 +380,7 @@ public class Guide : IDisposable
             W = Configuration.RenderedWidth,
             H = Configuration.RenderedHeight
         };
-        _ = SDL.RenderTexture(_renderer, _textureManager["guide"].SdlTexture, IntPtr.Zero, guideFRect);
+        _ = SDL.RenderTexture(_renderer, _textureManager[GuideTextureKey].SdlTexture, IntPtr.Zero, guideFRect);
 
         if (_showLogs)
         {
@@ -420,7 +395,7 @@ public class Guide : IDisposable
         _ = SDL.RenderPresent(_renderer);
     }
 
-    private void CheckIfQueueFilled()
+    private void FillRowTextureQueue()
     {
         while ((GetQueueHeight() - _guideRowBaseY) < Configuration.RenderedHeight)
         {
@@ -443,42 +418,40 @@ public class Guide : IDisposable
             _updateFPSFrameCounter = 30;
 
             var fps = 1000 / _frameTimes.Average();
-            using var surface = new Surface(TTF.RenderTextBlended(_fontManager["FiraCode"], $"{fps:0.00 FPS}", 0, Colors.Yellow));
-            _textureManager["fps"] = new Texture(SDL.CreateTextureFromSurface(_renderer, surface.SdlSurface));
+            using var surface = new Surface(TTF.RenderTextBlended(_fontManager[FiraCodeFontKey], $"{fps:0.00 FPS}", 0, Colors.Yellow));
+            _textureManager[FpsTextureKey] = new Texture(SDL.CreateTextureFromSurface(_renderer, surface.SdlSurface));
         }
         else
         {
             _updateFPSFrameCounter--;
         }
 
-        if (_textureManager["fps"] != null)
+        if (_textureManager[FpsTextureKey] == null)
+            return;
+
+        var texture = _textureManager[FpsTextureKey];
+
+        var lineHeight = _fontManager.FontConfigurations[FiraCodeFontKey].PointSize;
+
+        SDL.GetTextureSize(texture.SdlTexture, out var width, out var height);
+        var rect = new SDL.FRect
         {
-            var texture = _textureManager["fps"];
+            W = width,
+            H = height,
+            X = 0,
+            Y = Configuration.RenderedHeight - lineHeight * Configuration.Scale
+        };
 
-            var lineHeight = _fontManager.FontConfigurations["FiraCode"].PointSize;
-
-            SDL.GetTextureSize(texture.SdlTexture, out var width, out var height);
-            var rect = new SDL.FRect
-            {
-                W = width,
-                H = height,
-                X = 0,
-                Y = Configuration.RenderedHeight - lineHeight * Configuration.Scale
-            };
-
-            _ = SDL.RenderTexture(_renderer, texture.SdlTexture, IntPtr.Zero, rect);
-        }
-
+        _ = SDL.RenderTexture(_renderer, texture.SdlTexture, IntPtr.Zero, rect);
     }
 
     private void RenderLogs()
     {
-        const string firaCodeFontName = "FiraCode";
         if (_lastLinesLoggedCount != _containedLineLogger.LinesLogged)
         {
             _lastLinesLoggedCount = _containedLineLogger.LinesLogged;
 
-            var font = _fontManager[firaCodeFontName];
+            var font = _fontManager[FiraCodeFontKey];
             var yellow = new SDL.Color { A = 255, R = 255, G = 255, B = 0 };
 
             for (var i = 0; i < _containedLineLogger.Lines.Count; i++)
@@ -489,7 +462,7 @@ public class Guide : IDisposable
             }
         }
 
-        var lineHeight = _fontManager.FontConfigurations[firaCodeFontName].PointSize;
+        var lineHeight = _fontManager.FontConfigurations[FiraCodeFontKey].PointSize;
         var y = Configuration.WindowHeight - (lineHeight * Configuration.Scale * 50);
 
         for (var i = 0; i < _containedLineLogger.Lines.Count; i++)
